6e0cffaf76a41ca0602f87d8959e24e438a14760
Osee06 <oseeibobi@MacBook-Air-de-osee.local>
2024-10-15 22:28:44 +0200

    Second commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, je suis Macbook Air M3, j'aime le barça");
+
+	}
+
+}
+s
